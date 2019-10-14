440fe92928b2bb65d028954515197984e9e20313
DamasoDaniel <damasodt8a@gmail.com>
2019-10-13 22:29:34 -0500

    Primer ejercicio del curso

---
+++ Hola.java
@@ -0,0 +1,14 @@
+class Hola {
+    public static void main(String args[]){
+        byte primerNumero, segundoNumero, resultado;
+        
+        primerNumero = 8;
+        segundoNumero = 2;
+
+        resultado = (byte)(primerNumero + segundoNumero);
+
+        System.out.println(resultado);
+
+
+    }
+}
